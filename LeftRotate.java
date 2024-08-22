72fd921a17a06d2f84387ce03aedae1301ecdc27
Baikadi Sai Kiran Goud <154678643+BSaikiran22@users.noreply.github.com>
2024-08-22 23:06:06 +0530

    Add files via upload

---
+++ LeftRotate.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class LeftRotate {
+    static void slove(int arr[],int n){
+        int temp = arr[0];
+        for(int i = 0; i < n-1; i++){
+            arr[i] = arr[i+1];
+            }
+            arr[n-1] = temp;
+            for(int i=0; i<n;i++){
+                System.out.print(arr[i]+" ");
+            }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter size of array : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++){
+            System.out.println("Enter element at index "+i+" : ");
+            arr[i] = sc.nextInt();
+        }
+        slove(arr, n);
+
+
+    }
+}
